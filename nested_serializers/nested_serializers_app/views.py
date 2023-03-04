9e6537280bfa7cb81a96f18f12c13e76829202c5
glaucio.junior <glaucioripol@gmail.com>
2023-03-04 16:53:24 -0300

    adding order api

--- nested_serializers/nested_serializers_app/views.py
+++ nested_serializers/nested_serializers_app/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import (
     RetrieveUpdateDestroyAPIView
 )
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
@@ -15,18 +15,21 @@ class AuthorListView(ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     pagination_class = AuthorPagination
-    # * using django_filters
+    # * using django_filters, util for filtering per fields
     # filter_backends = [DjangoFilterBackend]
     # filterset_fields = ['first_name', 'last_name']
 
     # * using rest_framework filters
-    filter_backends = [SearchFilter]
+    filter_backends = [SearchFilter, OrderingFilter]
     # * prefix in fields and your function will be called
     # * "<field_name>" - contains
     # * "ˆ<field_name>" - starts with
     # * "=<field_name>" - exact match
     # * "@<field_name>" - full text search(Currently only supported Django's PostgreSQL backend.)
     search_fields = ['first_name', 'last_name']
+
+    # * ordering, filter by fields
+    ordering_fields = ['first_name', 'last_name']
 
 
 class AuthorDetailView(RetrieveUpdateDestroyAPIView):
