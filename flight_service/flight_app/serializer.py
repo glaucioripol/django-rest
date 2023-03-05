6d7801bc3ee9d524b278e959c4726370f65023c8
glaucio.junior <glaucioripol@gmail.com>
2023-03-05 13:10:01 -0300

    how to validate data on serializer

--- flight_service/flight_app/serializer.py
+++ flight_service/flight_app/serializer.py
@@ -1,12 +1,38 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from flight_app.models import Flight, Passenger, Reservation
+
+from re import match
+
+# * Validate the data before saving it to the database
+
+
+def validate_flight_data(data: dict):
+    flight_number = data['flight_number']
+    if (match(r'^\d$', str(flight_number)) == None):
+        raise serializers.ValidationError(
+            'Flight number must be a number'
+        )
+
+    return flight_number
 
 
 class FlightSerializer(ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
+        validators = [
+            validate_flight_data,
+        ]
+
+    def validate_flight_number(self, flight_number: int):
+        if (match(r'^\d$', str(flight_number)) == None):
+            raise serializers.ValidationError(
+                'Flight number must be a number'
+            )
+
+        return flight_number
 
 
 class ReservationSerializer(ModelSerializer):
